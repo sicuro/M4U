cd7d22f95f10326d0102200cd0875a8a7c8b708b
sicuro <gabriel.sicuro@gmail.com>
2020-04-27 18:30:37 -0300

    quem somos

--- js/script.js
+++ js/script.js
@@ -3,3 +3,57 @@ $('').toggleClass('');
 $(".menu-mobile, .close-menu").click(function(){
   $(".menu-mobile-open").toggleClass("show-menu");
 });
+
+$(".owl-carousel").owlCarousel({
+
+autoPlay: 3000,
+items : 2,
+itemsDesktop : [1199,3],
+itemsDesktopSmall : [979,3],
+center: true,
+nav:true,
+loop:true,
+responsive: {
+600: {
+items: 4
+}
+}
+
+});
+
+$(document).ready(function() {
+
+// Gets the video src from the data-src on each button
+
+var $videoSrc;  
+$('.video-btn').click(function() {
+    $videoSrc = $(this).data( "src" );
+});
+console.log($videoSrc);
+
+  
+  
+// when the modal is opened autoplay it  
+$('#myModal').on('shown.bs.modal', function (e) {
+    
+// set the video src to autoplay and not to show related video. Youtube related video is like a box of chocolates... you never know what you're gonna get
+$("#video").attr('src',$videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0" ); 
+})
+  
+
+
+// stop playing the youtube video when I close the modal
+$('#myModal').on('hide.bs.modal', function (e) {
+    // a poor man's stop video
+    $("#video").attr('src',$videoSrc); 
+}) 
+    
+    
+
+
+  
+  
+// document ready  
+});
+
+
